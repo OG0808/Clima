e23d2cbed83e2a4642cc01e0437459192b5a5ab4
OG0808 <ogarns@proton.me>
2023-07-03 02:15:37 -0500

    appClima

---
+++ src/App.jsx
@@ -0,0 +1,186 @@
+import * as React from "react";
+import { useState, useEffect, useRef } from "react";
+import ClimateCard from "./components/ClimateCard";
+import Switch from "@mui/material/Switch";
+import axios from "axios";
+import "./App.css";
+import { unsupportedProp } from "@mui/material";
+import Loading from "./components/Loading";
+
+function App() {
+  const [position, setPosition] = useState({});
+  console.log(position.lon);
+
+  //! loader ----------//
+  const [loading, setLoading] = useState(true);
+
+  // const [positionDefoult, setPositionDefoult] = useState('')
+
+  //* manejo y captura de la informacion del input para enviarla a la api y buscar la ciudad selecionada
+  const [inputValue, setInputValue] = useState("bogota");
+
+  // const cityDefoult =() => {
+  //   if (inputValue === '') {
+  //      return 'Bogota'
+  //   }
+  // }
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value.trim());
+  };
+  const [city, setCity] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCity(inputValue);
+  };
+
+  //? logica para el Dark mode -----------------------------------------//
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  //!Estados en general para manejo de peticiones y botones de la interfas grafica
+
+  const [changeGrade, setChangeGrade] = useState(true);
+
+  const changeGrades = () => {
+    setChangeGrade(changeGrade == false);
+  };
+
+  const [positionDevice, setPositionDivice] = useState();
+
+  const [positionStart, setPositionStart] = useState();
+
+  const iconData = positionStart?.weather[0].icon;
+
+  //! Funcion para obtener la ubicacion del dispositivo de donde se este abriendo la app
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=5&appid=61f7f94e8821c4e346c3d9ca5e7cde9e`
+      )
+      .then((response) => {
+        setPositionDivice(response.data);
+        setLoading(false);
+      })
+      .then((error) => console.log(error));
+
+    //? esta funncion es por si queremos tener la ubicacion por defecto del lugar donde estemos como tal
+    function success(pos) {
+      const obj = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude,
+      };
+
+      setPosition(obj);
+    }
+    navigator.geolocation.getCurrentPosition(success);
+  }, [inputValue]);
+
+  useEffect(() => {
+    if (positionDevice) {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${
+            positionDevice?.[0].lat
+          }&lon=${
+            positionDevice?.[0].lon 
+          }&appid=61f7f94e8821c4e346c3d9ca5e7cde9e`
+        )
+        .then((response) => {
+          setPositionStart(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [city]);
+
+  //! Funcion que retorna depediendo el clima un icono indicado
+  //! tomando como referencia la propieda wather del objeto
+
+  return (
+    <div className="main">
+      {loading && <Loading />}
+      <div
+        className={checked ? "main__container" : "main__containerDark"}
+        id="light"
+      >
+        <div className="card_search__and__switch">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+          </form>
+          <Switch
+            checked={checked}
+            onChange={handleChange}
+            sx={{
+              width: 52,
+              height: 26,
+              padding: 0,
+              "& .MuiSwitch-switchBase": {
+                padding: 0,
+                margin: 0.4,
+                transitionDuration: "300ms",
+                "&.Mui-checked": {
+                  transform: "translateX(25px)",
+                  color: checked ? "#53388f" : "#53388f",
+                  "& + .MuiSwitch-track": {
+                    backgroundColor: checked ? "#fff" : "#53388f",
+                    opacity: 1,
+                    border: 0,
+                  },
+                  "&.Mui-disabled + .MuiSwitch-track": {
+                    opacity: 0.5,
+                  },
+                },
+                "&.Mui-focusVisible .MuiSwitch-thumb": {
+                  color: "#53388f",
+                  border: "6px solid #fff",
+                },
+                "&.Mui-disabled .MuiSwitch-thumb": {
+                  color: "#53388f",
+                },
+                "&.Mui-disabled + .MuiSwitch-track": {
+                  opacity: 0.7,
+                },
+              },
+              "& .MuiSwitch-thumb": {
+                boxSizing: "border-box",
+                width: 20,
+                height: 20,
+              },
+              "& .MuiSwitch-track": {
+                borderRadius: 26 / 2,
+                backgroundColor: checked ? "#E9E9EA" : "#53388f",
+                opacity: 1,
+                transition: "background-color 500ms",
+              },
+            }}
+          />
+        </div>
+
+        <ClimateCard
+          icon={iconData}
+          allData={positionStart}
+          hadleGrades={changeGrade}
+          checkedDark={checked}
+        />
+        <button
+          onClick={changeGrades}
+          className={checked ? "change__grades" : "change__gradesDark"}
+        >
+          {changeGrade ? "Cambiar a °F" : "Cambiar a °C"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ClimateCard.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import imagen1 from "/Recurso 1.svg";
+// import imagen2 from "./Recurso 2.svg";
+// import imagen3 from "./Recurso 3.svg";
+import imagen4 from "/Recurso 4.svg";
+import imagen5 from "/Recurso 5.svg";
+import imagen6 from "/Recurso 6.svg";
+import imagen7 from "/Recurso 7.svg";
+// import imagen8 from "./Recurso 8.svg";
+// import imagen9 from "./Recurso 9.svg";
+import imagen10 from "/Recurso 10.svg";
+// import imagen11 from "./Recurso 11.svg";
+import imagen12 from "/Recurso 12.svg";
+import imagen13 from "/Recurso 13.svg";
+// import imagen14 from "./Recurso 14.svg";
+import imagen15 from "/Recurso 15.svg";
+import imagen16 from "/Recurso 16.svg";
+// import imagen17 from "./Recurso 17.svg";
+// import imagen18 from "./Recurso 18.svg";
+// import imagen19 from "./Recurso 19.svg";
+// import imagen20 from "./Recurso 20.svg";
+// import imagen21 from "./Recurso 21.svg";
+import imagen22 from "/Recurso 22.svg";
+// import imagen23 from "./Recurso 23.svg";
+import imagen26 from "/Recurso 26.svg";
+
+const ClimateCard = ({ icon, allData, hadleGrades, checkedDark }) => {
+  console.log();
+  //! Condicion para mostrar una
+  //!imagen en svg dependiendo del codigo del icono que evnie la api
+  //! no encontre una forma mas dinamica para imprtar las imagenes y hacer las condiciones para mostrar las imagenes
+  function iconToDisplay() {
+    if (icon == "01d") return imagen1;
+    if (icon == "02d") return imagen12;
+    if (icon == "03d") return imagen5;
+    if (icon == "04d") return imagen6;
+    if (icon == "09d") return imagen10;
+    if (icon == "10d") return imagen15;
+    if (icon == "11d") return imagen7;
+    if (icon == "13d") return imagen22;
+    if (icon == "50d") return imagen16;
+    if (icon == "01n") return imagen13;
+    if (icon == "02n") return imagen4;
+    if (icon == "03n") return imagen5;
+    if (icon == "04n") return imagen6;
+    if (icon == "09n") return imagen10;
+    if (icon == "10n") return imagen26;
+    if (icon == "11n") return imagen7;
+    if (icon == "13n") return imagen22;
+    if (icon == "50n") return imagen16;
+  }
+
+  //! Convertimos los grados Kelvin a Celcius o Centigrado y a  Fahrenheit.
+
+  const Celcius = allData?.main.temp - 273.15;
+  const Fahrenheit = ((allData?.main.temp - 273) * 9) / 5 + 32;
+
+  return (
+    <>
+      <div className="card">
+        <div className="card_grade_and_sun">
+          <div className="card__image_clima">
+            <img src={iconToDisplay()} alt="" />
+          </div>
+          <div className={checkedDark ? "card__gadre" : 'card__gadreDark '}>
+            <h1>{hadleGrades ? Celcius.toFixed(1) : Fahrenheit.toFixed(1)}{hadleGrades ? '°c' : '°f' }</h1>
+            <div className={checkedDark ? "card__temp__description" : "card__temp__descriptionDark"}>
+              <p>VIENTO <span style={{ marginLeft: '120px' }}/>{allData?.wind.deg} m/s</p>
+              <br />
+              <p>NUBES<span style={{ marginLeft: '130px' }}/>{allData?.clouds.all} %</p>
+              <br />
+              <p>PRESION <span style={{ marginLeft: '110px' }}>{allData?.main.pressure} hPa</span></p>
+            </div>
+          </div>
+        </div>
+        <div className={checkedDark ? "card_city" :"card_cityDark"}>
+          <h2>{allData?.name}, {allData?.sys.country}</h2>
+          <p>{allData?.weather[0].description}</p>
+        </div>
+      </div>
+    </>
+  );
+
+  //{`https://openweathermap.org/img/wn/${iconToDisplay(icon)}@2x.png`}
+};
+
+export default ClimateCard;
